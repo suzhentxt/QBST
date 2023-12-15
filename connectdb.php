bde053b318f455570673d71c3608f2688e2144f9
suzhentxt <chaunguyentotrinh2006@gmail.com>
2023-12-16 01:44:00 +0700

    update

---
+++ connectdb.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','qbst') or die('connection failed');
+
+?>
--- templates/connectdb.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','','qbst') or die('connection failed');
-
-?>
